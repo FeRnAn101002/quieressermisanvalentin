ebb12983158d2d7e49862748c1464118dba0a494
FeRnAn101002 <157319514+FeRnAn101002@users.noreply.github.com>
2024-02-09 10:47:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja';
+    document.getElementById('messageContainer').innerHTML = '¡Sabia que dirias que si!, ahora escribeme jeje';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
